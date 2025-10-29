a999f70a2934ab7fcebd81f166a7823a5d2724ee
Paris Vigay <143814237+parisvigay@users.noreply.github.com>
2025-10-29 15:53:50 +0000

    Update useScrollToSection.js

--- src/hooks/useScrollToSection.js
+++ src/hooks/useScrollToSection.js
@@ -1,16 +1,18 @@
+import { useCallback } from "react";
+
 export default function useScrollToSection() {
-    const scrollToSection = (id) => {
-        const section = document.getElementById(id)
-        const navBar = document.getElementById("navBar")
+  const scrollToSection = useCallback((id) => {
+    const section = document.getElementById(id);
+    const navBar = document.getElementById("navBar");
 
-        if (section && navBar) {
-            const offset = section.offsetTop - navBar.offsetHeight;
-            window.scrollTo({
-                top: offset,
-                behavior: "smooth",
-            });
-        }
-    };
+    if (section && navBar) {
+      const offset = section.offsetTop - navBar.offsetHeight;
+      window.scrollTo({
+        top: offset,
+        behavior: "smooth",
+      });
+    }
+  }, []);
 
-    return scrollToSection;
+  return scrollToSection;
 }
